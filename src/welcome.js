ca78f47bbcfea65fe53e7c450187a904cb0b9c8b
John Tomaselli <jrt@gtz.com>
2018-07-10 15:39:55 -0400

    v11

--- src/welcome.js
+++ src/welcome.js
@@ -1,7 +1,7 @@
 //import {computedFrom} from 'aurelia-framework';
 
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App! v10';
+  heading = 'Welcome to the Aurelia Navigation App! v11';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
