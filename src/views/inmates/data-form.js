45c05d47adc8458f26c17e0e69e8fa4f9eeffc87
John Tomaselli <jrt@gtz.com>
2018-07-12 19:20:42 -0400

    f

--- src/views/inmates/data-form.js
+++ src/views/inmates/data-form.js
@@ -2,7 +2,7 @@ import { Router } from 'aurelia-router';
 import { inject } from 'aurelia-dependency-injection';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { ApplicationService } from '../../services/application-service';
-
+import moment from 'moment';
 // import { ApiService } from '../../utils/servicesApi';
 // import { ApplicationService } from '../../services/application-service';
 // import { MyDataService } from "../../services/my-data-service";
