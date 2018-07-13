c75d43f9e02b95684c7e3b0a80f3a9c983a2c7af
John Tomaselli <jrt@gtz.com>
2018-07-13 13:56:34 -0400

    s

--- src/views/inmates/data-form.js
+++ src/views/inmates/data-form.js
@@ -57,7 +57,7 @@ export class DataForm {
     if (flag) this.appService.currentRecord = booking
     this.bookingDate = '';
     this.classification = '';
-    this.getServices(booking, index)
+    this.getServices(booking, 0)
   }
   addService() {
     let service = this.services
@@ -72,7 +72,7 @@ export class DataForm {
     service.unshift(item)
     if (flag) this.services = service
     this.serviceDateFrom = '';
-     this.getInvoices(service, index)
+     this.getInvoices(service, 0)
   }
   addInvoice() {
     let invoice = this.invoices
