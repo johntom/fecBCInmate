9e4776bb4320acfe9b87d9b3fbeb70433ab751a7
John Tomaselli <jrt@gtz.com>
2018-07-15 10:22:59 -0400

    a

--- src/welcome.js
+++ src/welcome.js
@@ -3,7 +3,7 @@ import { ApiService } from './utils/servicesApi';
 import { ApplicationService } from './services/application-service';
 @inject(ApplicationService, ApiService)
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App! v47z';
+  heading = 'Welcome to the Aurelia Navigation App! v48a';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
@@ -21,8 +21,17 @@ export class Welcome {
 
   activate() {
     console.log('in activate')
-    let cCodes = [{ id: 1, code: 'a1' }, { id: 2, code: 'a2' }, { id: 3, code: 'a3' }, { id: 4, code: 'a4' }]
+     let cCodes = [{ id: 1, code: 'County' }, { id: 2, code: 'ICE' }, { id: 3, code: 'State' }, { id: 4, code: 'Fed' }]
     this.appService.classificationList = cCodes //" model.bind="opt.code
+    let cCodes2 = [{ id: 1, code: 'Outpatient' }, { id: 2, code: 'Emergency' }, { id: 3, code: 'Inpatient' }]
+    this.appService.serviceprovidedList = cCodes2 
+
+    let cCodes3 = [{ id: 1, code: 'Jail Transport' }, { id: 2, code: 'Ambulance Service' }, { id: 3, code: 'Other Agency' }]
+    this.appService.transportList = cCodes3 
+    let cCodes4 = [{ id: 1, code: 'Medical' }, { id: 2, code: 'Mental Health' }]
+    this.appService.servicetypeList = cCodes4 
+    let cCodes5 = [{ id: 1, code: 'Approved' }, { id: 2, code: 'Denied' }]
+    this.appService.approvedList = cCodes5 
   }
 
 
