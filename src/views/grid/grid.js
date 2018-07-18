8161061eb70666fd479809c546bea3c19db64716
John Tomaselli <jrt@gtz.com>
2018-07-18 10:49:38 -0400

    r

--- src/views/grid/grid.js
+++ src/views/grid/grid.js
@@ -18,27 +18,18 @@ export class Grid {
     pageSizes: true,
     buttonCount: 10
   };
-  constructor(api) {
-    // this.datasource = {
-    //   type: 'odata',
-    //   transport: {
-    //     read: '//demos.telerik.com/kendo-ui/service/Northwind.svc/Customers'
-    //   },
-    //   pageSize: 5
-    // };
- heading = 'Inmates...';
-
-  this.datasource = new kendo.data.DataSource({
+  heading = 'Inmates...';
+  datasource = new kendo.data.DataSource({
     transport: {
       read: (options) => {
         //  this.loadData(this.capColor, this.prevtown)
         this.loadData()
-          .then((claim) => {
-            console.log(' inv datasource ', claim.length);// inv[0]);
-            options.success(claim);
+          .then((inmates) => {
+            console.log(' inv datasource ', inmates.length);// inv[0]);
+            options.success(inmates);
           });
       },
-      
+
     },
     schema: {
       model: {
@@ -54,34 +45,95 @@ export class Grid {
     //   { field: "template", aggregate: "count" }
     // ]
   })
-}
+  constructor(api) {
+    // this.datasource = {
+    //   type: 'odata',
+    //   transport: {
+    //     read: '//demos.telerik.com/kendo-ui/service/Northwind.svc/Customers'
+    //   },
+    //   pageSize: 5
+    // };
+    this.api = api
 
 
-    activate() {
-      console.log('in activate')
-
-      // return this.api.getInmates()
-      //   .then(jsonRes => {
-      //     this.inmates = jsonRes.data
-      //     this.allinmates = jsonRes.data
-      //     console.log('inmates ', this.inmates)
-      //   });
-    }
-  
- loadData() {
-    console.log('this.loadData ')
-   return this.api.getInmatesExpanded()
-        .then(jsonRes => {
-          this.inmates = jsonRes.data
-         
-         this.allinmates = jsonRes.data
-          console.log('inmates ', this.inmates)
-        });
-
-    
-    }
   }
 
 
+  activate() {
+    console.log('in activate')
+    // this.datasource = new kendo.data.DataSource({
+    //   transport: {
+    //     read: (options) => {
+    //       //  this.loadData(this.capColor, this.prevtown)
+    //       this.loadData()
+    //         .then((inmates) => {
+    //           console.log(' inv datasource ', inmates.length);// inv[0]);
+    //           options.success(inmates);
+    //         });
+    //     },
 
+    //   },
+    //   schema: {
+    //     model: {
+    //       id: "id", // Must assign id for update to work
+    //       // fields: {
+    //       //   // LegacyID: { type: "number" }, // scan template
+    //       //   Artist: { type: "string" }, // barcode insured
+    //       // }
+    //     }
+    //   },
+    //   pageSize: 12,
+    //   // aggregate: [{ field: "type", aggregate: "count" },
+    //   //   { field: "template", aggregate: "count" }
+    //   // ]
+    // })
+    // // return this.api.getInmates()
+    // //   .then(jsonRes => {
+    // //     this.inmates = jsonRes.data
+    // //     this.allinmates = jsonRes.data
+    // //     console.log('inmates ', this.inmates)
+    // //   });
+  }
+
+  loadData() {
+    console.log('this.loadData ')
+    //  return Promise.all([
+    //     this.dataService.loadSearch(this.queryParams)
+    //   ]).then(values => {
+    //     this.origItems = values[0];
+    //     // this.appService.searchDataLoaded = true;
+    //     claim = this.origItems;
+    //     // console.log(' this.loadSearch', this.origItems)
+    //     console.log('claim ', claim.length)
+    //     return claim
+    //     //bad   this.currentItem = this.items.find(f => f.id == params.id);
+    //   }).catch(error => {
+    //     console.error("Error encountered while trying to get data.", error);
+    //   });
+    //     this.api.getInmates()
+    //       .then(jsonRes => {
+    //         this.inmates = jsonRes.data
+
+    //         this.allinmates = jsonRes.data
+    //         console.log('inmates ', this.inmates)
+    // return Promise.resolve(this.inmates);
+    //       });
+    let claim;
+    return Promise.all([
+      this.api.getInmatesExpanded()
+
+    ]).then(values => {
+      this.inmates = values[0].data;
+      // this.appService.searchDataLoaded = true;
+      claim = this.inmates;
+      // console.log(' this.loadSearch', this.origItems)
+      console.log('claim ', claim.length)
+      return claim
+      //bad   this.currentItem = this.items.find(f => f.id == params.id);
+    }).catch(error => {
+      console.error("Error encountered while trying to get data.", error);
+    });
+  }
 }
+
+
