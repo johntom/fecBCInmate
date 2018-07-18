ebf86733a8ff6c0069f8e012dc926de54e970563
John Tomaselli <jrt@gtz.com>
2018-07-18 11:28:15 -0400

    u

--- src/views/grid/grid.js
+++ src/views/grid/grid.js
@@ -41,7 +41,7 @@ export class Grid {
             type: "date"
           },
           bookingDate: {
-            type: "date"
+            type: "date" 
           },
           sentencingDate: {
             type: "date"
