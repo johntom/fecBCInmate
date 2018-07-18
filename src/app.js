f04075d3ba9509aefbf2ca85f2dc172f205ed79b
John Tomaselli <jrt@gtz.com>
2018-07-18 14:26:57 -0400

    c

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import {PLATFORM} from 'aurelia-pal';
 
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia Babel SystemJS';
+    config.title = 'Inmate Medical'//Aurelia Babel SystemJS';
     config.map([
       {"route": ["", "welcome"], "name": "welcome", "moduleId": PLATFORM.moduleName("./welcome"), "nav": true, "title": "Welcome"},
       // {"route": "users", "name": "users", "moduleId": PLATFORM.moduleName("./users"), "nav": true, "title": "GitHub Users"},
