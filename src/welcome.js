7525c75dffcb5e24740dd33e1228736aa736bd10
John Tomaselli <jrt@gtz.com>
2018-07-20 17:51:48 -0400

    v52a

--- src/welcome.js
+++ src/welcome.js
@@ -3,7 +3,7 @@ import { ApiService } from './utils/servicesApi';
 import { ApplicationService } from './services/application-service';
 @inject(ApplicationService, ApiService)
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App! v51j-revert to 50u';
+  heading = 'Welcome to the Aurelia Navigation App! v52a v51j-revert to 50u';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
