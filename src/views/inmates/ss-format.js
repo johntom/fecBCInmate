92c94412445e91d768805ed7f1832070afcde990
John Tomaselli <jrt@gtz.com>
2018-07-26 11:36:52 -0400

    Removed src/views/inmates/ss-format.js

--- src/views/inmates/ss-format.js
+++
@@ -1,9 +0,0 @@
- import numeral from 'numeral';
-
-// export class CurrencyFormatValueConverter {
-export class SsFormatValueConverter {
-  toView(value) {
-     return numeral(value).format('(000-00-0000)');
-    //return 'ss'// (value).format('(000-00-0000)');
-  }
-}
