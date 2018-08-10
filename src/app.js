440c4717d646cd2a0f787cf2793904fcdf33b995
John Tomaselli <jrt@gtz.com>
2018-08-10 13:54:42 -0400

    v3L26redo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 
+// export class App {
+//   configureRouter(config, router) {
+//     config.title = 'Aurelia Babel SystemJS';
+//     config.map([
+//       {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome/welcome", "nav": true, "title": "Welcome"},
+//       {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"}
+//       // {"route": "bootstrap-select-demo", "name": "bootstrap-select-demo", "moduleId": "./views/bootstrap-select-demo/bootstrap-select-demo", "nav": true, "title": "Bootstrap Select Demo"}
+//     ]);
+
+//     this.router = router;
+//   }
+// }
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia Babel SystemJS';
+    config.title = 'Inmate Medical'//Aurelia Babel SystemJS';
     config.map([
-      {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome/welcome", "nav": true, "title": "Welcome"},
-      {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"}
-      // {"route": "bootstrap-select-demo", "name": "bootstrap-select-demo", "moduleId": "./views/bootstrap-select-demo/bootstrap-select-demo", "nav": true, "title": "Bootstrap Select Demo"}
+      { "route": ["", "welcome"], "name": "welcome", "moduleId": PLATFORM.moduleName("./welcome"), "nav": true, "title": "Welcome" },
+      { "route": "grid", "name": "grid", "moduleId": PLATFORM.moduleName("./views/grid/grid"), "nav": true, "title": "Imates Grid" },
+      { "route": "inmates", "name": "inmates", "moduleId": PLATFORM.moduleName("./views/inmates/inmates"), "nav": true, "title": "Inmates" },
+      { "route": "inmatesdata", "name": "inmatesdata", "moduleId": PLATFORM.moduleName("./views/inmates/data-form"), "nav": false, "title": "InmatesData" },
+      { "route": "inmates/:id", "name": "inmates-data-form", "moduleId": PLATFORM.moduleName("./views/inmates/data-form"), "nav": false, "title": "InmatesData" },
+      { "route": "payee", "name": "payee", "moduleId": PLATFORM.moduleName("./views/payee/payee"), "nav": true, "title": "Payee" },
+
+
+      { "route": "ab-select", "name": "ab-select", "moduleId": PLATFORM.moduleName("./views/ab-select/ab-select"), "nav": true, "title": "AB Select" }
+
+
     ]);
 
     this.router = router;
