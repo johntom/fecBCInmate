413360db1f4abb7a855d39600f64542185a02515
John Tomaselli <jrt@gtz.com>
2018-08-10 13:49:51 -0400

    Removed src/views/welcome/welcome.js

--- src/views/welcome/welcome.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-export class Welcome {
-  
-  constructor() {
-  let nDate = moment().format('MM/DD/YYYY')
-    this.heading = '1 - Welcome to the Aurelia Navigation App - 2w ' + nDate
-  }
-
-}
